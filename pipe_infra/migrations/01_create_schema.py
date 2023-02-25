71a16d70753d92dcf034d4e768edc9aaf43ad8f1
Nana Bediako <dconesoko@gmail.com>
2023-02-25 16:17:55 -0330

    add data migration script

---
+++ pipe_infra/migrations/01_create_schema.py
@@ -0,0 +1,10 @@
+"""
+Defining and creating schema
+
+"""
+
+from yoyo import step
+
+__depends__={}
+
+steps=[step("CREATE SCHEMA bitcoin","DROP SCHEMA bitcoin CASCADE")]
---
+++ pipe_infra/table_creation.py
@@ -0,0 +1,18 @@
+from yoyo import get_backend,read_migrations
+import sys
+from dotenv import load_dotenv
+load_dotenv(".wh_env")
+sys.path.append("/Users/bediako/Desktop/Data_engineering/pipe_infra/src")
+
+from App_code.utils import config
+from App_code.utils.db import WarehouseConnection,DBConnection
+
+con_data:DBConnection = config.get_warehouse_creds()
+con_str:str = WarehouseConnection(con_data).conn_url
+
+migrations=read_migrations("./migrations")
+backend=get_backend(con_str)
+
+with backend.lock():
+    backend.apply_migrations(backend.to_apply(migrations))
+    print("done applying migrations")
